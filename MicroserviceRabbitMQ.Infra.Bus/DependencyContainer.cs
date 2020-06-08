832801db4cae33bd6bbbcedd402ec6a6546c6f7a
Asit <asit199@gmail.com>
2020-06-08 21:17:33 +0530

    Initial commit of Dependency commit project

---
+++ MicroserviceRabbitMQ.Infra.Bus/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using MicroserviceRabbitMQ.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroserviceRabbitMQ.Infra.Bus
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
